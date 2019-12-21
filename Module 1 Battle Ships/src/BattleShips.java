8c5f1b59a12a1c6d1274debe6d2c7d8a819e2a69
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2019-12-20 23:33:25 -0500

    initial commit

---
+++ Module 1 Battle Ships/src/BattleShips.java
@@ -0,0 +1,9 @@
+public class BattleShips {
+    public static void main (String [] args){
+           System.out.println("**** Welcome to Battle Ships game *****");
+           System.out.println();
+           System.out.println("Right now, the sea is empty");
+
+
+    }
+}
