db7e681bd3d0fad2d3afa42b5559473113e6c8bc
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2019-12-23 19:51:24 -0500

    Add files via upload

---
+++ src/BattleShips.java
@@ -0,0 +1,42 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class BattleShips {
+    public static void main (String [] args){
+        // declaring and initializing 2D array
+         String map[][] = new String[10][10];
+         //populate map with sea (empty space)
+        for(int r = 0; r < map.length; r++){
+            for (int c = 0; c < map[0].length; c++){
+                map[r][c] = " ";
+            }
+        }
+
+        String playerShip = "@";
+        String computerShip = "2";
+
+        map[2][2]= playerShip;
+        map[2][4]= playerShip;
+        map[4][2]= playerShip;
+        map[5][2]= computerShip;
+        map[7][5]= computerShip;
+        map[9][7]= computerShip;
+
+        System.out.println("**** Welcome to Battle Ships game *****");
+           System.out.println();
+           System.out.println("Right now, the sea is empty");
+        System.out.println();
+           System.out.println("   0123456789   ");
+
+
+        for(int r = 0; r < map.length; r++){
+            String joinedString = (String.join("", map[r]));
+            System.out.println(r+" |"+joinedString+"| "+r);
+            }
+        System.out.println("   0123456789   ");
+
+    }
+
+    }
+
